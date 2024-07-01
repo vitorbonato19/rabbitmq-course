b55f6666648a08e90d19b632528de427096d3900
Vitor Bonato <vitorbonatocam@gmail.com>
2024-06-30 22:04:33 -0300

    Dto implementation

--- src/main/java/com/java/rabbitmq_springweb/controller/dto/PropostaRequestDto.java
+++ src/main/java/com/java/rabbitmq_springweb/controller/dto/PropostaRequestDto.java
@@ -1,4 +1,94 @@
 package com.java.rabbitmq_springweb.controller.dto;
 
 public class PropostaRequestDto {
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String telefone;
+
+    private String cpf;
+
+    private Double renda;
+
+    private String valorSolicitado;
+
+    private Integer prazoPagamento;
+
+    public PropostaRequestDto() {
+
+    }
+
+    public PropostaRequestDto(String nome,
+                              String sobrenome,
+                              String telefone,
+                              String cpf,
+                              Double renda,
+                              String valorSolicitado,
+                              Integer prazoPagamento) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.renda = renda;
+        this.valorSolicitado = valorSolicitado;
+        this.prazoPagamento = prazoPagamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(Double renda) {
+        this.renda = renda;
+    }
+
+    public String getValorSolicitado() {
+        return valorSolicitado;
+    }
+
+    public void setValorSolicitado(String valorSolicitado) {
+        this.valorSolicitado = valorSolicitado;
+    }
+
+    public Integer getPrazoPagamento() {
+        return prazoPagamento;
+    }
+
+    public void setPrazoPagamento(Integer prazoPagamento) {
+        this.prazoPagamento = prazoPagamento;
+    }
 }
--- src/main/java/com/java/rabbitmq_springweb/controller/dto/PropostaResponseDto.java
+++ src/main/java/com/java/rabbitmq_springweb/controller/dto/PropostaResponseDto.java
@@ -1,4 +1,130 @@
 package com.java.rabbitmq_springweb.controller.dto;
 
 public class PropostaResponseDto {
+
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String telefone;
+
+    private String cpf;
+
+    private Double renda;
+
+    private String valorSolicitado;
+
+    private Integer prazoPagamento;
+
+    private Boolean aprovado;
+
+    private String observacao;
+
+    public PropostaResponseDto() {
+
+    }
+
+    public PropostaResponseDto(Long id,
+                               String nome,
+                               String sobrenome,
+                               String telefone,
+                               String cpf,
+                               Double renda,
+                               String valorSolicitado,
+                               Integer prazoPagamento,
+                               Boolean aprovado,
+                               String observacao) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.renda = renda;
+        this.valorSolicitado = valorSolicitado;
+        this.prazoPagamento = prazoPagamento;
+        this.aprovado = aprovado;
+        this.observacao = observacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(Double renda) {
+        this.renda = renda;
+    }
+
+    public String getValorSolicitado() {
+        return valorSolicitado;
+    }
+
+    public void setValorSolicitado(String valorSolicitado) {
+        this.valorSolicitado = valorSolicitado;
+    }
+
+    public Integer getPrazoPagamento() {
+        return prazoPagamento;
+    }
+
+    public void setPrazoPagamento(Integer prazoPagamento) {
+        this.prazoPagamento = prazoPagamento;
+    }
+
+    public Boolean getAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(Boolean aprovado) {
+        this.aprovado = aprovado;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
 }
