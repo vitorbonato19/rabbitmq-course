aef2b03c40ec334153ddf82fdf7b860daad35501
Vitor Bonato <vitorbonatocam@gmail.com>
2024-07-01 21:05:42 -0300

    Feat. Following Rest's good practices

--- src/main/java/com/java/rabbitmq_springweb/controller/PropostaController.java
+++ src/main/java/com/java/rabbitmq_springweb/controller/PropostaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/proposta")
@@ -23,6 +24,9 @@ public class PropostaController {
     @PostMapping
     public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto) {
         PropostaResponseDto response = service.criar(requestDto);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").
+                buildAndExpand(response.getId())
+                .toUri()).body(response);
     }
 }
